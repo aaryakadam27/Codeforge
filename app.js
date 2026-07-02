bd712bc2e5be7f5004fd4a0a7fcc9ce1c2a7609e
aaryakadam27 <kadamaarya74@gmail.com>
2026-07-02 18:27:18 +0530

    git ignore

---
+++ app.js
@@ -0,0 +1,17 @@
+
+import express from "express";
+
+
+const app = express();
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+
+app.get("/", (req, res) => {
+  res.json({ message: "Hello, World!" });
+});
+
+export default app;
